3f52d68d442ad3bafa747d07a9078d7a9fdcaa77
mat <matthew.kelcey@gmail.com>
2010-02-16 20:36:34 +1100

    sparse vector multiply

--- take2/array_extensions_spec.rb
+++ take2/array_extensions_spec.rb
@@ -9,19 +9,42 @@ describe 'array extensions test' do
    it 'should calc distance from array to another array of same indexes' do
       a = [1,1, 2,2]
       b = [1,2, 2,3]
-      a.sparse_distance_to(b).should == Math.sqrt(2)
+      a.sparse_distance_to(b).should == Math.sqrt(1+1)
    end
 
-   it 'should calc distance from array to another array of same length with differing indexes, first array idx less' do
+   it 'should calc distance from array to another array of same length with differing indexes' do
       a = [1,1, 2,2]
       b = [1,2, 3,3]
-      a.sparse_distance_to(b).should == Math.sqrt(14)
+      a.sparse_distance_to(b).should == Math.sqrt(1+4+9)
+      b.sparse_distance_to(a).should == Math.sqrt(1+4+9)
    end
 
-   it 'should calc distance from array to another array of same length with differing indexes, first array idx more' do
+   it 'should calc distance from array to another array of differing length' do
       a = [1,1, 4,2]
-      b = [1,2, 3,3]
-      a.sparse_distance_to(b).should == Math.sqrt(14)
+      b = [1,2, 3,3, 4,3]
+      a.sparse_distance_to(b).should == Math.sqrt(1+9+1)
+      b.sparse_distance_to(a).should == Math.sqrt(1+9+1)
+   end
+
+   it 'should calc distance from array to another array of differing length' do
+      a = [1,1, 4,2]
+      b = [1,2, 3,3, 4,3]
+      a.sparse_distance_to(b).should == Math.sqrt(1+9+1)
+      b.sparse_distance_to(a).should == Math.sqrt(1+9+1)
+   end
+
+   it 'should calc distance from array to another array of differing length with different final indexes' do
+      a = [1,1, 4,2]
+      b = [1,2, 3,3, 5,3]
+      a.sparse_distance_to(b).should == Math.sqrt(1+9+4+9)
+      b.sparse_distance_to(a).should == Math.sqrt(1+9+4+9)
+   end
+
+   it 'should calc distance from array to another array of differing length with different initial indexes' do
+      a = [2,1, 4,2]
+      b = [1,2, 3,3, 4,3]
+      a.sparse_distance_to(b).should == Math.sqrt(4+1+9+1)
+      b.sparse_distance_to(a).should == Math.sqrt(4+1+9+1)
    end
 
 end
